38f925305b385e9b7310800c7692222759304407
folix <iwei1982.tan@gmail.com>
2026-07-29 14:43:45 +0000

    update

--- app/code/MslPay/LianLian/Gateway/Request/Builder/CustomerBuilder.php
+++ app/code/MslPay/LianLian/Gateway/Request/Builder/CustomerBuilder.php
@@ -43,12 +43,13 @@ class CustomerBuilder implements BuilderInterface
         $phone = $billingAddress ? (string) $billingAddress->getTelephone() : '';
 
         /* firstName/lastName 为空时从 email 提取：取 @ 前缀，用 '.' 或 '_' 分割为 first/last */
-        if (empty($firstName) && empty($lastName)) {
+        if (empty($firstName)) {
             $emailLocal = strstr($email, '@', true) ?: $email;
             $parts = preg_split('/[._]/', $emailLocal, 2);
             $firstName = $parts[0] ?? $emailLocal;
-            $lastName = $parts[1] ?? $firstName;
+            $lastName = $lastName ??  ($parts[1] ?? $firstName);
         }
+      
 
         /* 构建 Customer DataObject */
         $customer = new Customer();
